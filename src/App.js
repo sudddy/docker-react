6618508916c154186a5f6ee8830b971e292086bc
sudddy <suddpro@gmail.com>
2022-01-06 15:21:22 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          annan eduthakaren one!
         </a>
       </header>
     </div>
